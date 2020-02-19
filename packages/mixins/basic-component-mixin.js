a1461e758fa7a896f06079f7bfa96daa116f20ba
DevinShi2019 <yutian.shi@foxmail.com>
2020-02-19 03:09:03 +0000

    feat(form-builder): 增加组件的事件穿透
    让组件的事件穿透到表单外部

--- packages/mixins/basic-component-mixin.js
+++ packages/mixins/basic-component-mixin.js
@@ -2,19 +2,36 @@
  * @Author: DevinShi
  * @Date: 2020-02-11 06:49:10
  * @LastEditors: DevinShi
- * @LastEditTime: 2020-02-17 08:46:30
+ * @LastEditTime: 2020-02-19 03:06:35
  * @Description: file content description
  */
 export default {
     props: {
         label: {
-            type: String, 
+            type: String
         },
         labelColor: {
-            type: String, 
+            type: String
         },
         extraProp: {
             type: Object
+        },
+        uuid: {
+            type: String 
+        },
+        componentName: {
+            type: String
+        }
+    },
+    methods: {
+        $formEventEmit(eventName, event) {            
+            this.$emit(eventName, event);
+            this.$emit('formEventEmit', {
+                eventName: eventName,
+                uuid: this.uuid,
+                componentName: this.componentName,
+                event: event
+            });
         }
     }
 }
